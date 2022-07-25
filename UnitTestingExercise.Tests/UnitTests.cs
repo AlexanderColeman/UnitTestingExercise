9b890326f823ed49eba0548e2236326df94dfb0e
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-25 15:15:14 -0500

    added more test cases

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -8,6 +8,9 @@ namespace UnitTestingExercise.Tests
     {
         [Theory]
         [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(2, 0, 5, 7)] //Add test data <-------
+        [InlineData(50, 23, 5, 78)] //Add test data <-------
+        [InlineData(-20, 15, 5, 0)] //Add test data <-------
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
@@ -31,6 +34,9 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(10,5,5)]//Add test data <-------
+        [InlineData(10,9,1)]//Add test data <-------
+        [InlineData(2,10,-8)]//Add test data <-------
+        [InlineData(0,10,-10)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
@@ -47,6 +53,9 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(5,2,10)]//Add test data <-------
+        [InlineData(5,5,25)]//Add test data <-------
+        [InlineData(4,5,20)]//Add test data <-------
+        [InlineData(3,0,0)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -64,6 +73,9 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(20,5,4)]//Add test data <-------
+        [InlineData(0,5,0)]//Add test data <-------
+        [InlineData(5,0,0)]//Add test data <-------
+        [InlineData(-5,5,-1)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
--- UnitTestingExercise/Calculator.cs
+++ UnitTestingExercise/Calculator.cs
@@ -51,6 +51,10 @@ namespace UnitTestingExercise
 
         public int Divide(int x, int y)
         {
+            if(x == 0 || y == 0)
+            {
+                return 0;
+            }
             return x / y;
         }
 
